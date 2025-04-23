b0d0c412798d34e88eb8505f85bd7fb9b76f4758
KingOfRaccoon <zvezdakov2015@yandex.ru>
2025-04-23 17:02:03 +0300

    add index.html

--- rc_backend/rc_app/views/public.py
+++ rc_backend/rc_app/views/public.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(
+        request,
+        "rc_app/public/index.html",
+        {
+            "foo": "bar",
+        },
+        # content_type="application/html",
+    )
