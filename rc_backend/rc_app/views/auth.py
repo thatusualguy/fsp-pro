f6a8215b4840390a4ddae946217f834a160adffa
KingOfRaccoon <zvezdakov2015@yandex.ru>
2025-04-24 21:22:05 +0300

    add registration form to the registration page

--- rc_backend/rc_app/views/auth.py
+++ rc_backend/rc_app/views/auth.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
@@ -13,7 +15,7 @@ class LoginUser(LoginView):
     template_name = 'rc_app/login/login.html'
     form_class = AuthenticationForm
     # redirect_authenticated_user = True
-    # success_url = reverse_lazy('/')
+    success_url = reverse_lazy('/app/profile/')
 
     def form_valid(self, form):
         username = form.cleaned_data.get('username')
@@ -33,19 +35,43 @@ class LoginUser(LoginView):
             return self.form_invalid(form)
 
 
+class CustomRegisterForm(UserCreationForm):
+    username = forms.CharField(label="Логин", max_length=150, required=True)
+    first_name = forms.CharField(label="Имя пользователя", max_length=150, required=True)
+    last_name = forms.CharField(label="Фамилия", max_length=150, required=True)
+    email = forms.EmailField(label="Email", required=True)
+    fsp = forms.ModelChoiceField(label="ФСП", queryset=FSP.objects.all(), required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'fsp')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['username']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
 class RegisterUser(FormView):
-    template_name = 'register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    template_name = 'rc_app/login/register.html'
+    form_class = CustomRegisterForm
+    success_url = reverse_lazy('/app/login')
 
     def form_valid(self, form):
-        # Save the new user
         user = form.save()
-
-        # Create a Profile for the new user
-        Profile.objects.create(user=user, fsp=FSP.objects.first())
-
-        # Redirect to login page
+        # Create a Profile for the new user with all the provided information
+        Profile.objects.create(
+            user=user,
+            name=form.cleaned_data['first_name'],
+            last_name=form.cleaned_data['last_name'],
+            email=form.cleaned_data['email'],
+            fsp=form.cleaned_data['fsp'],
+        )
         return redirect(self.success_url)
 
 
