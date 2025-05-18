92b1008d8533af9392198a616fadca86fd49a141
KingOfRaccoon <zvezdakov2015@yandex.ru>
2025-05-18 22:35:36 +0300

    fix show details

--- src/core/views/regions/region_detail.py
+++ src/core/views/regions/region_detail.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 from src.core.models import Region
 
@@ -6,3 +7,8 @@ class RegionDetailView(DetailView):
     model = Region
     template_name = 'core/region_detail.html'
     context_object_name = 'region'
+    slug_field = 'code'  # Поле модели для поиска
+    slug_url_kwarg = 'code'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Region, code=self.kwargs['code'])
