a77fba6f199b28ad8fd0810cdd5c7906daad3dd2
Maksim Rybakov <m.p.rybakov@gmail.com>
2025-05-18 22:54:18 +0300

    fix urls

--- src/core/views/regions/urls.py
+++ src/core/views/regions/urls.py
@@ -7,7 +7,7 @@ app_name = 'regions'
 urlpatterns = [
     path('', RegionListView.as_view(), name='list'),
     path('create/', RegionCreateView.as_view(), name='create'),
-    path('<int:id>/', RegionDetailView.as_view(), name='detail'),
+    path('<int:pk>/', RegionDetailView.as_view(), name='detail'),
     path('<int:pk>/update/', RegionUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', RegionDeleteView.as_view(), name='delete'),
 ]
